0e7eb4ceae7f22b1887516ebed5f87e1a2ef2db5
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-31 23:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
- return document.querySelector('#nested.element')
+ return document.querySelector('#nested.target')
 }
 
 function increaseRankBy(n) {

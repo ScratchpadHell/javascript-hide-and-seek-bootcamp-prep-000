4134babc97c30fa0f6edeff5350a6800bdc62588
Jacques Bellisario <gypsyoak@yahoo.com>
2018-04-01 00:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   for (let i = 0, l = rankFind.length; i < l; i++) {
    let children = rankFind[i].children
    
-  for(let j = 0, k = children.length; if j < k; j++)
+  for(let j = 0, k = children.length; j < k; j++)
   children[j].innerHTML = parseInt(children[j].innerHTML) + n
   }
 }

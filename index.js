9884908f31a1e7d2951a6731cd92411101510c68
Jacques Bellisario <gypsyoak@yahoo.com>
2018-04-01 00:58:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,10 @@ function increaseRankBy(n) {
 function deepestChild() {
   let node = getElementById('grand-node')
   let nextNode = node.children[0]
+  
+  while(nextNode)
+  node = nextNode
+  nextNode = node.children[0]
+  
+  return node
 }

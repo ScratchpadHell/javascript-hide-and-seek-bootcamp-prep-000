f9c8cb1c163dc82758ce568df4f840fea814699e
Jacques Bellisario <gypsyoak@yahoo.com>
2018-04-01 00:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let node = getElementById('grand-node')
+  let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
   
   while(nextNode) {
